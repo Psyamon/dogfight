bae30db31b0d2e2a96218b06c8d0bf9484df75d0
Julie <Julie@10.167.50.110>
2016-05-24 14:52:38 +0200

    phase 2

---
+++ src/jpu2016/dogfight/model/Dimension.java
@@ -0,0 +1,33 @@
+package jpu2016.dogfight.model;
+
+public class Dimension {
+	private int width;
+	private int height;
+	
+	public Dimension(int width, int height){
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Dimension(Dimension dimension){
+		this.height = dimension.height;
+		this.width = dimension.width;
+	}
+	
+	public int getWidth(){
+		
+	}
+	
+	public void setWidth(int width){
+		
+	}
+	
+	public int getHeight(){
+		
+	}
+	
+	public void setHeight(int height){
+		
+	}
+
+}
