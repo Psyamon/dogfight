b7ae1055b2cda28637403fb69e6c9ea448e6a026
FranzRoth <francois.roth@viacesi.fr>
2016-05-24 15:29:45 +0200

    Phase 2

---
+++ src/jpu2016/dogfight/view/GraphicsBuilder.java
@@ -0,0 +1,7 @@
+package jpu2016.dogfight.view;
+
+/**
+ * Created by Franz on 24/05/2016.
+ */
+public class GraphicsBuilder {
+}
---
+++ src/jpu2016/gameframe/GameFrame.java
@@ -0,0 +1,7 @@
+package jpu2016.gameframe;
+
+/**
+ * Created by Franz on 24/05/2016.
+ */
+public class GameFrame {
+}
