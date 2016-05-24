bf756f689364c4c7b5d9b95bf9d2cd79057b4e96
Julie <Julie@10.167.50.110>
2016-05-24 15:08:50 +0200

    phase 2 final

--- src/jpu2016/dogfight/model/Dimension.java
+++ src/jpu2016/dogfight/model/Dimension.java
@@ -15,19 +15,19 @@ public class Dimension {
 	}
 	
 	public int getWidth(){
-		
+		return this.width;
 	}
 	
 	public void setWidth(int width){
-		
+		this.width = width;
 	}
 	
 	public int getHeight(){
-		
+		return this.height;
 	}
 	
 	public void setHeight(int height){
-		
+		this.height = height;
 	}
 
 }
